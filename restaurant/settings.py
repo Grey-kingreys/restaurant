92888d7f185a3947364ccbd8af10f2fbb9809b42
unknown <soulmamoudou0@gmail.com>
2026-01-07 23:59:20 +0000

    Apres quelques modification pour le fonctionnement de tailwind essai 2

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -154,11 +154,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-# Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 # Media files
